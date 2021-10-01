a6261864f017d6352e6af5f68ba3229c94e05bdc
João Vitor R <joaovitor_new2@live.com>
2021-10-01 17:30:42 -0300

    teste de GIF + Dialog

--- ColorCoffee/koffee/lib/view/color/color_page.dart
+++ ColorCoffee/koffee/lib/view/color/color_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../theme/theme.dart';
 import '../../model/roast.dart';
 import 'color_controller.dart';
 
@@ -12,6 +13,7 @@ class ColorPage extends StatefulWidget {
 class _ColorPageState extends State<ColorPage> {
   ColorControll controller = ColorControll();
   Color? containerColor = const Color(0xFFFFF9F2);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +28,12 @@ class _ColorPageState extends State<ColorPage> {
             builder: (BuildContext context, Roast? value, __) {
               return Center(
                 child: value == null
-                    ? const Text(
-                        'Envie foto',
+                    ? Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/gifs/enquadrarFotoAndroid.gif',
+                          width: 200.0,
+                        ),
                       )
                     : Column(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -60,8 +66,14 @@ class _ColorPageState extends State<ColorPage> {
         padding: const EdgeInsets.only(bottom: 62),
         child: FloatingActionButton(
           onPressed: () async {
-            await controller.getRandomColor();
-            changeTheme();
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return const AtencaoDialog();
+                });
+
+            // await controller.getRandomColor();
+            // changeTheme();
           },
           tooltip: 'Send Image',
           child: const Icon(Icons.camera_enhance_rounded),
@@ -74,5 +86,64 @@ class _ColorPageState extends State<ColorPage> {
     setState(() {
       containerColor = controller.color.withAlpha(210);
     });
+  }
+}
+
+class AtencaoDialog extends StatefulWidget {
+  const AtencaoDialog({Key? key}) : super(key: key);
+
+  @override
+  _AtencaoDialogState createState() => _AtencaoDialogState();
+}
+
+class _AtencaoDialogState extends State<AtencaoDialog> {
+  final ScrollController _scrollController = ScrollController();
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0)), //this right here
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Scrollbar(
+          controller: _scrollController,
+          isAlwaysShown: true,
+          child: ListView(
+            controller: _scrollController,
+            shrinkWrap: true,
+            children: [
+              const Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text(
+                    'Atenção',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500),
+                  )),
+              const Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text(
+                      'Para que haja um bom reconhecimento da amostra de café, recorte a imagem preenchendo o quadrado com a amostra.',
+                      style: TextStyle(color: Colors.black, fontSize: 14.0))),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  'assets/gifs/enquadrarFotoAndroid.gif',
+                  width: 200.0,
+                ),
+              ),
+              ElevatedButton(
+                child: const Text('Continuar'),
+                style: AppTheme.elevatedButtonStyle,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
