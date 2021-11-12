96821fdc9a8ecb5f84631704ab1a4be75ec0b4a1
João Vitor R <joaovitor_new2@live.com>
2021-11-12 20:29:18 -0300

    removendo arquivos não usados

--- API/api.py
+++ API/api.py
@@ -1,5 +1,45 @@
-from modules import app
+from flask import Flask, request, jsonify
+import os
+# from datetime import datetime
+import random
 
-#TODO: Change to False
+app = Flask(__name__)
 
-app.run(debug=False, host='0.0.0.0', port=33)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/random', methods=['GET'])
+def randomColor():
+    return jsonify({
+        "color": "color",
+        "prediction": "{}".format(random.randint(0, 100)),
+        "confidence": "{}".format(random.randint(0, 100)),
+        "rgb": [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+    }), 200
+
+
+@app.route('/analisys', methods=['POST'])
+def analisys():
+    if request.method == 'POST':
+
+        r = int(request.json['r'])
+        g = int(request.json['g'])
+        b = int(request.json['b'])
+
+        h = request.json['h']
+        s = request.json['s']
+        v = request.json['v']
+        
+        print(h,s,v)
+
+        return jsonify({
+            "color": "color",
+            "prediction": "55",
+            "confidence": "96",
+            "rgb": [r, b, g]
+        }), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
