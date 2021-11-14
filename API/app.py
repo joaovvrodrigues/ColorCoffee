4ce95f4127ea3f9e50fd4c59f212480fe8bef469
João Vitor R <joaovitor_new2@live.com>
2021-11-14 18:35:16 -0300

    Xô devolver o orgulho do gueto

--- API/app.py
+++ API/app.py
@@ -26,7 +26,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return """
+<p>
+Eu quero problemas fúteis, preocupações inúteis <br>
+Só Deus sabe o quanto eu lutei, vendo o mundo dizer: Não tem <br>
+(Hum, bem, é passado, passado) <br> <br>
+Bom, hoje eles diz: Cê tem o dom (Obrigado, obrigado) <br>
+Fui quem obedece, agora eu sou quem manda <br>
+Vou ver o que acontece, mas lá da varanda <br>
+É tipo um trampo social, sério  <br> <br>
+Tô dando a chance de ver como nasce um império <br>
+Com o brilho dos diamante que eles tanto diz <br>
+Multiplicado 10 mil vez no olhar feliz das minhas menina <br> 
+Sente o clima, eu posso comprar Hennessy, mas prefiro tubaína <br> <br>
+Aos que apontam, eu trago a resposta mais louca <br>
+Não diz que eu sou marrento não <br>
+Trampa mais e melhor que eu, cala a boca <br> <br>
+Xô devolver o orgulho do gueto <br>
+E dar outro sentido pra frase: Tinha que ser preto <br>
+
+<br><br><br> Emicida
+</p>    
+        """
 
 
 @app.route('/random', methods=['GET'])
@@ -63,5 +84,5 @@ def analisys():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=33) # Dev
-    app.run(threaded=True, port=5000) # Produção
+    app.run(debug=True, host='0.0.0.0', port=33) # Dev
+    # app.run(threaded=True, port=5000)  # Produção
